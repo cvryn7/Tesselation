41536af5f33f5c93a5797344ff7d3a741176e81f
karanbhagat <karanbhagat@karanbhagat-PC>
2015-04-09 01:07:55 -0400

    files added

---
+++ src/cgShape.java
@@ -0,0 +1,110 @@
+//
+//  cgShape.java
+//
+//  Class that includes routines for tessellating a number of basic shapes.
+//
+//  Students are to supply their implementations for the functions in
+//  this file using the function "addTriangle()" to do the tessellation.
+//
+
+import java.awt.*;
+import java.nio.*;
+import java.awt.event.*;
+import javax.media.opengl.*;
+import javax.media.opengl.awt.GLCanvas;
+import java.io.*;
+
+
+public class cgShape extends simpleShape
+{
+    /**
+     * constructor
+     */
+    public cgShape()
+    {
+    }
+
+    /**
+     * makeCube - Create a unit cube, centered at the origin, with a given
+     * number of subdivisions in each direction on each face.
+     *
+     * @param subdivision - number of equal subdivisons to be made in each
+     *        direction along each face
+     *
+     * Can only use calls to addTriangle()
+     */
+    public void makeCube (int subdivisions)
+    {
+        if( subdivisions < 1 )
+            subdivisions = 1;
+
+        // YOUR IMPLEMENTATION HERE
+    }
+
+    /**
+     * makeCylinder - Create polygons for a cylinder with unit height, centered
+     * at the origin, with separate number of radial subdivisions and height
+     * subdivisions.
+     *
+     * @param radius - Radius of the base of the cylinder
+     * @param radialDivision - number of subdivisions on the radial base
+     * @param heightDivisions - number of subdivisions along the height
+     *
+     * Can only use calls to addTriangle()
+     */
+    public void makeCylinder (float radius, int radialDivisions, int heightDivisions)
+    {
+        if( radialDivisions < 3 )
+            radialDivisions = 3;
+
+        if( heightDivisions < 1 )
+            heightDivisions = 1;
+
+        // YOUR IMPLEMENTATION HERE
+    }
+
+    /**
+     * makeCone - Create polygons for a cone with unit height, centered at the
+     * origin, with separate number of radial subdivisions and height
+     * subdivisions.
+     *
+     * @param radius - Radius of the base of the cone
+     * @param radialDivision - number of subdivisions on the radial base
+     * @param heightDivisions - number of subdivisions along the height
+     *
+     * Can only use calls to addTriangle()
+     */
+    public void makeCone (float radius, int radialDivisions, int heightDivisions)
+    {
+        if( radialDivisions < 3 )
+            radialDivisions = 3;
+
+        if( heightDivisions < 1 )
+            heightDivisions = 1;
+
+        // YOUR IMPLEMENTATION HERE
+    }
+
+    /**
+     * makeSphere - Create sphere of a given radius, centered at the origin,
+     * using spherical coordinates with separate number of thetha and
+     * phi subdivisions.
+     *
+     * @param radius - Radius of the sphere
+     * @param slides - number of subdivisions in the theta direction
+     * @param stacks - Number of subdivisions in the phi direction.
+     *
+     * Can only use calls to addTriangle
+     */
+    public void makeSphere (float radius, int slices, int stacks)
+    {
+        if( slices < 3 )
+            slices = 3;
+
+        if( stacks < 3 )
+            stacks = 3;
+
+        // YOUR IMPLEMENTATION HERE
+    }
+
+}
